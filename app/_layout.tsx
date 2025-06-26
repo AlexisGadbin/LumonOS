4528a92ac0249589d0986bf8ecc1456dbc2a5a55
Alexis Gadbin <alexis@gadbin.com>
2025-06-26 09:50:11 +0200

    fix: hid status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { AppTheme } from "./theme";
 
@@ -16,6 +17,7 @@ export default function RootLayout() {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar hidden={true} />
       <ThemeProvider value={AppTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
